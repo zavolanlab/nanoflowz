87b2c0bb59f3115f69cf47fe90f3df8e07654e39
aleksei.mironov.scicore <magmir71@gmail.com>
2026-05-06 14:51:33 +0200

    updated readme and count_reads.sh

--- bin/count_reads.sh
+++ bin/count_reads.sh
@@ -1,21 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SAMPLE=$1
-STEP=$2
-BAM=$3
-CPUS=$4
-OUT=$5
+sample_id=$1
+step_name=$2
+bam_file=$3
+cpus=$4
+out_file=$5
 
-# Extract read name and whether it mapped to a chromosome (RNAME != *)
-samtools view -@ $CPUS -F 2048 $BAM | awk '{if($3=="*") print $1"\tUN"; else print $1"\tMA"}' | sort -S 2G | uniq -c > counts.tmp
+# 1. Total unique reads 
+# (cut -f1 extracts the read_id, sort -u deduplicates them)
+total_reads=$(samtools view -@ "$cpus" "$bam_file" | cut -f1 | LC_ALL=C sort -u | wc -l)
 
-# Sum up the occurrences
-UNMAPPED=$(awk '$3=="UN" {sum++} END {print sum+0}' counts.tmp)
-UM=$(awk '$1==1 && $3=="MA" {sum++} END {print sum+0}' counts.tmp)
-MM=$(awk '$1>1 && $3=="MA" {sum++} END {print sum+0}' counts.tmp)
-TOTAL=$((UNMAPPED + UM + MM))
+# 2. Unmapped reads 
+# (awk checks if RNAME (col 3) is '*' and POS (col 4) is '0' based on SAM specs)
+unmapped_reads=$(samtools view -@ "$cpus" "$bam_file" | awk -F'\t' '$3 == "*" && $4 == 0 {print $1}' | LC_ALL=C sort -u | wc -l)
 
-# Output as TSV row
-echo -e "${SAMPLE}\t${STEP}\t${TOTAL}\t${UNMAPPED}\t${UM}\t${MM}" > $OUT
+# 3. Mapped reads (Unique vs Multi-mapped)
+# Extract read IDs where RNAME (col 3) is NOT '*', then count how many times each ID appears
+samtools view -@ "$cpus" "$bam_file" | awk -F'\t' '$3 != "*" {print $1}' | LC_ALL=C sort | uniq -c > mapped_counts.txt
 
-rm counts.tmp
+# If a read ID appears exactly 1 time, it is a unique mapper
+unique_reads=$(awk '$1 == 1' mapped_counts.txt | wc -l)
+
+# If a read ID appears > 1 time, it is a multi-mapper (e.g. split/supplementary alignments)
+mm_reads=$(awk '$1 > 1' mapped_counts.txt | wc -l)
+
+# Clean up intermediate file
+rm mapped_counts.txt
+
+# Output to the exact TSV file requested by Nextflow
+echo -e "${sample_id}\t${step_name}\t${total_reads}\t${unmapped_reads}\t${unique_reads}\t${mm_reads}" > "$out_file"
